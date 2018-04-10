34e7dc70d0713b0b6d84d84f4c402abdf57430e8
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-04-09 20:11:35 -0700

    login signup and logout working

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { Link, nativeHistory,
-        Route, Router, StackRoute, withRouter } from 'react-router-native';
 import styling from '../styles/style';
 
 class Header extends React.Component {
